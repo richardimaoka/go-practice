8f4194c808ebc95aa19a25b34d04c10bd514f509
richardimaoka <richard.s.imaoka@gmail.com>
2025-08-30 18:55:43 +0900

    root flag

--- cmd/root.go
+++ cmd/root.go
@@ -31,14 +31,9 @@ func Execute() {
 	}
 }
 
+var verbose bool
+
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.go-practice.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
